936985c1121d0da9ee0beae6aedcfb48fa96e50c
Kamari Senters <82468798+MeMariYo@users.noreply.github.com>
2021-06-03 11:21:49 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 if(isset($_GET['FIRSTNAME'])){//show data
-   echo $_GET['FIRSTNAME'];
+   echo $_GET['FIRSTNAME'] . ' ' . $_GET['LASTNAME'];
 }else{//show form
   echo '
   <form action="">
    First Name: <input type="text" name="FIRSTNAME"/><br />
+   Last Name: <input type="text" name="LASTNAME"/><br />
    <input type="submit" />
   </form>
   
